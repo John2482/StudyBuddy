a8dea18830748fa3bced0d23371a0c3b92a3afa4
jcrotzer <jcrot002@odu.edu>
2018-11-11 16:46:43 -0500

    Inital Commit

---
+++ src/com/studybuddy/entity/User.java
@@ -0,0 +1,32 @@
+package com.studybuddy.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable  {
+	@Id @GeneratedValue
+	Long id;
+	String userName;
+	String passWord;
+	static final long serialVersionUID = 1L;
+	protected User (){
+		
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	
+}
